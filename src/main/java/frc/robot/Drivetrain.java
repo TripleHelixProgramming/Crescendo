92f037a28ace9a2305c2cd2e76106d722f37857a
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2024-01-07 13:20:10 -0500

    Changed to CANSparkMax

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -16,21 +16,12 @@ public class Drivetrain {
   public static final double kMaxSpeed = 3.0; // 3 meters per second
   public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
 
-  //private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
-  //private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
-  //private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
-  //private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
-
   private final SwerveModule m_frontLeft = new SwerveModule(1, 2, 0, 1, 2, 3);
   private final SwerveModule m_frontRight = new SwerveModule(3, 4, 4, 5, 6, 7);
   private final SwerveModule m_backLeft = new SwerveModule(5, 6, 8, 9, 10, 11);
   private final SwerveModule m_backRight = new SwerveModule(7, 8, 12, 13, 14, 15);
 
   private final AnalogGyro m_gyro = new AnalogGyro(0);
-
-  //private final SwerveDriveKinematics m_kinematics =
-  //    new SwerveDriveKinematics(
-  //        m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
 
   private final SwerveDriveOdometry m_odometry =
       new SwerveDriveOdometry(
