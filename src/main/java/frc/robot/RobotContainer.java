b3c0f66c91e18b31558f0fa724aa3177f43f8af9
Nate Laverdure <nate.laverdure@gmail.com>
2024-01-15 18:28:10 -0500

    getfieldrelative from button state of controller object

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,7 @@ public class RobotContainer {
 
   public RobotContainer() {
 
-    m_swerve.setDefaultCommand(new ZorroDrive(m_swerve, m_driver, true));
+    m_swerve.setDefaultCommand(new ZorroDrive(m_swerve, m_driver));
 
     // Create a button on Smart Dashboard to reset the encoders.
     SmartDashboard.putData(
@@ -34,9 +34,6 @@ public class RobotContainer {
 
     new JoystickButton(m_driver, OIConstants.kZorroDIn)
         .onTrue(new InstantCommand(() -> m_swerve.resetGyro()).ignoringDisable(true));
-
-    new JoystickButton(m_driver, OIConstants.kZorroEDown)
-        .whileTrue(new ZorroDrive(m_swerve, m_driver, false));
   }
 
   public Command getAutonomousCommand() {
