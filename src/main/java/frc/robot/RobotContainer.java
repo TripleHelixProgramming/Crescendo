008f63aa679ff4f227cc3cdf9538ae652a6ecff1
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2024-01-16 20:47:51 -0500

    Added pneumatic commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,15 @@ package frc.robot;
 // import static edu.wpi.first.wpilibj.XboxController.Button;
 
 import edu.wpi.first.wpilibj.Joystick;
-// import edu.wpi.first.wpilibj.XboxController;
-// import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 // import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
+import frc.robot.arm.Arm;
 import frc.robot.drivetrain.Drivetrain;
 import frc.robot.drivetrain.ZorroDrive;
 
@@ -20,7 +21,11 @@ public class RobotContainer {
 
   private final Drivetrain m_swerve = new Drivetrain();
 
+  private final Arm m_arm = new Arm();
+
   private Joystick m_driver = new Joystick(OIConstants.kDriverControllerPort);
+
+  private XboxController m_operator = new XboxController(OIConstants.kOperatorControllerPort);
 
   // private final XboxController m_controller = new
   // XboxController(OIConstants.kDriverControllerPort);
@@ -36,6 +41,12 @@ public class RobotContainer {
 
     new JoystickButton(m_driver, OIConstants.kZorroDIn)
         .onTrue(new InstantCommand(() -> m_swerve.resetGyro()).ignoringDisable(true));
+
+    new JoystickButton(m_operator, Button.kLeftBumper.value)
+        .onTrue(new InstantCommand(() -> m_arm.pneumaticRetract()));
+
+    new JoystickButton(m_operator, Button.kRightBumper.value)
+        .onTrue(new InstantCommand(() -> m_arm.pneumaticDeploy()));
   }
 
   public Command getAutonomousCommand() {
