aa1f3c23269059a8bd6e68f834fed860460593a6
bot3O3 <75286864+bot3O3@users.noreply.github.com>
2024-01-16 21:06:53 -0500

    Added Auto builder to drivetrain subsystem. Currently not working.

--- src/main/java/frc/robot/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/drivetrain/Drivetrain.java
@@ -3,6 +3,11 @@
 package frc.robot.drivetrain;
 
 import com.kauailabs.navx.frc.AHRS;
+// auto impors
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
@@ -59,6 +64,8 @@ public class Drivetrain extends SubsystemBase {
             m_rearRight.getPosition()
           });
 
+  public AutoBuilder bob;
+
   public Drivetrain() {
     resetGyro();
 
@@ -67,6 +74,28 @@ public class Drivetrain extends SubsystemBase {
       module.initializeAbsoluteTurningEncoder();
       module.initializeRelativeTurningEncoder();
     }
+
+    // sets up auto builder
+    bob = new AutoBuilder();
+    bob.configureHolonomic(
+        this::getPose, // Robot pose supplier
+        this::resetPose, // Method to reset odometry (will be called if your auto has a starting
+        // pose)
+        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE
+        // ChassisSpeeds
+        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in
+            // your Constants class
+            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+            new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+            4.5, // Max module speed, in m/s
+            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+            new ReplanningConfig() // Default path replanning config. See the API for the options
+            // here
+            ),
+        true,
+        this // Reference to this subsystem to set requirements
+        );
   }
 
   public void resetGyro() {
