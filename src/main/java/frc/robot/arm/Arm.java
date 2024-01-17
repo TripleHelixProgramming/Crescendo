9516c396b50992284161bc0066656b59ce409d5a
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2024-01-16 19:15:21 -0500

    added Arm subsystem

---
+++ src/main/java/frc/robot/arm/Arm.java
@@ -0,0 +1,137 @@
+package frc.robot.arm;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.RobotConstants;
+
+public class Arm {
+    static double kMaxSpeed = Constants.DriveConstants.kMaxTranslationalVelocity;
+    static double kMaxAngularSpeed = Constants.DriveConstants.kMaxRotationalVelocity;
+
+    private final SwerveModule m_frontLeft =
+    new SwerveModule(
+        "FrontLeft",
+        DriveConstants.MotorControllers.kFrontLeftDriveMotorPort,
+        DriveConstants.MotorControllers.kFrontLeftTurningMotorPort,
+        DriveConstants.AbsoluteEncoders.kFrontLeftTurningEncoderPort);
+    private final SwerveModule m_frontRight =
+    new SwerveModule(
+        "FrontRight",
+        DriveConstants.MotorControllers.kFrontRightDriveMotorPort,
+        DriveConstants.MotorControllers.kFrontRightTurningMotorPort,
+        DriveConstants.AbsoluteEncoders.kFrontRightTurningEncoderPort);
+    private final SwerveModule m_rearLeft =
+    new SwerveModule(
+        "RearLeft",
+        DriveConstants.MotorControllers.kRearLeftDriveMotorPort,
+        DriveConstants.MotorControllers.kRearLeftTurningMotorPort,
+        DriveConstants.AbsoluteEncoders.kRearLeftTurningEncoderPort);
+    private final SwerveModule m_rearRight =
+    new SwerveModule(
+        "RearRight",
+        DriveConstants.MotorControllers.kRearRightDriveMotorPort,
+        DriveConstants.MotorControllers.kRearRightTurningMotorPort,
+        DriveConstants.AbsoluteEncoders.kRearRightTurningEncoderPort);
+
+    private SwerveModule[] modules = {m_frontLeft, m_frontRight, m_rearLeft, m_rearRight};
+
+    private final AHRS m_gyro = new AHRS();
+
+    private final SwerveDriveOdometry m_odometry =
+    new SwerveDriveOdometry(
+        DriveConstants.kDriveKinematics,
+        m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        });
+
+    public Drivetrain() {
+    resetGyro();
+
+    for (SwerveModule module : modules) {
+    module.resetDriveEncoder();
+    module.initializeAbsoluteTurningEncoder();
+    module.initializeRelativeTurningEncoder();
+    }
+    }
+
+    public void resetGyro() {
+    m_gyro.reset();
+    }
+
+    @Override
+    public void periodic() {
+    for (SwerveModule module : modules) {
+    SmartDashboard.putNumber(
+        module.getName() + "RelativeTurningPosition",
+        module.getRelativeTurningPosition().getDegrees());
+
+    SmartDashboard.putNumber(
+        module.getName() + "AbsoluteTurningPosition",
+        module.getAbsTurningPosition().getDegrees());
+
+    SmartDashboard.putNumber(
+        module.getName() + "RelativeDrivePosition", module.getRelativeDrivePosition());
+
+    SmartDashboard.putNumber(
+        module.getName() + "AbsoluteMagnetOffset",
+        module.getAbsTurningEncoderOffset().getDegrees());
+    }
+
+    SmartDashboard.putNumber("GyroAngle", m_gyro.getRotation2d().getDegrees());
+    }
+
+    /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param chassisSpeeds x, y, and theta speeds
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+    public void drive(ChassisSpeeds chassisSpeeds, boolean fieldRelative) {
+    var swerveModuleStates =
+        DriveConstants.kDriveKinematics.toSwerveModuleStates(
+            ChassisSpeeds.discretize(chassisSpeeds, RobotConstants.kPeriod));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_rearLeft.setDesiredState(swerveModuleStates[2]);
+    m_rearRight.setDesiredState(swerveModuleStates[3]);
+    }
+
+  /** Updates the field relative position of the robot. */
+    public void updateOdometry() {
+    m_odometry.update(
+        m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
+        m_frontLeft.getPosition(),
+        m_frontRight.getPosition(),
+        m_rearLeft.getPosition(),
+        m_rearRight.getPosition()
+        });
+    }
+
+  /** Reconfigures all swerve module steering angles using external alignment device */
+    public void zeroAbsTurningEncoderOffsets() {
+    for (SwerveModule module : modules) {
+    module.zeroAbsTurningEncoderOffset();
+    }
+    }
+
+    /**
+   * @return The heading of the robot
+   */
+    public Rotation2d getHeading() {
+    return m_gyro.getRotation2d();
+    }
+}
