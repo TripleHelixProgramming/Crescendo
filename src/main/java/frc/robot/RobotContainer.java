5a2f03f55850b854768b8bcc0d7558624799c0be
Nate Laverdure <nate.laverdure@gmail.com>
2024-01-18 21:32:19 -0500

    create hasGamePiece method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -58,7 +58,7 @@ public class RobotContainer {
     // Intake Note from floor
     new JoystickButton(m_operator, Button.kX.value)
         .whileTrue(new RunCommand(() -> m_intake.setVelocity(1.0))
-        .until(m_intake.m_noteSensor::get)
+        .until(m_intake::hasGamePiece)
         .onlyIf(lowerArmCommand::isScheduled));
 
     // Shift Note further into Intake
