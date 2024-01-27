9f09986dd2c747d184cd0f5f5b6dd1cc024a395d
Nate Laverdure <nate.laverdure@gmail.com>
2024-01-27 14:05:13 -0500

    uncommented arm and intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,11 +21,11 @@ import frc.robot.intake.Intake;
 public class RobotContainer {
 
   private final Drivetrain m_swerve = new Drivetrain();
-  //private final Arm m_arm = new Arm();
-  //private final Intake m_intake = new Intake();
+  private final Arm m_arm = new Arm();
+  private final Intake m_intake = new Intake();
 
   private Joystick m_driver = new Joystick(OIConstants.kDriverControllerPort);
-  //private XboxController m_operator = new XboxController(OIConstants.kOperatorControllerPort);
+  private XboxController m_operator = new XboxController(OIConstants.kOperatorControllerPort);
 
   private Auto m_selectedAuto;
 
@@ -37,7 +37,7 @@ public class RobotContainer {
     m_swerve.setDefaultCommand(new ZorroDrive(m_swerve, m_driver, getAlliance()));
     m_swerve.configurePathPlanner();
 
-    //m_intake.setDefaultCommand(m_intake.createStopIntakeCommand());
+    m_intake.setDefaultCommand(m_intake.createStopIntakeCommand());
 
     // Create a button on Smart Dashboard to reset the encoders.
     SmartDashboard.putData("Align Encoders",
@@ -49,27 +49,27 @@ public class RobotContainer {
         .onTrue(new InstantCommand(() -> m_swerve.resetGyro())
             .ignoringDisable(true));
 
-    // Command lowerArmCommand = m_arm.createLowerArmCommand();
-    // Command raiseArmCommmand = m_arm.createRaiseArmCommand();
-    // // Operator controller buttons
-    // new JoystickButton(m_operator, Button.kLeftBumper.value).onTrue(lowerArmCommand);
-    // new JoystickButton(m_operator, Button.kRightBumper.value).onTrue(raiseArmCommmand);
+    Command lowerArmCommand = m_arm.createLowerArmCommand();
+    Command raiseArmCommmand = m_arm.createRaiseArmCommand();
+    // Operator controller buttons
+    new JoystickButton(m_operator, Button.kLeftBumper.value).onTrue(lowerArmCommand);
+    new JoystickButton(m_operator, Button.kRightBumper.value).onTrue(raiseArmCommmand);
 
-    // // Intake Note from floor
-    // new JoystickButton(m_operator, Button.kX.value)
-    //     .whileTrue((m_intake.createSetVelocityCommand(1.0))
-    //     .until(m_intake::hasGamePiece)
-    //     .onlyIf(lowerArmCommand::isScheduled));
+    // Intake Note from floor
+    new JoystickButton(m_operator, Button.kX.value)
+        .whileTrue((m_intake.createSetVelocityCommand(1.0))
+        .until(m_intake::hasGamePiece)
+        .onlyIf(lowerArmCommand::isScheduled));
 
-    // // Shift Note further into Intake
-    // new JoystickButton(m_operator, Button.kA.value)
-    //     .onTrue((m_intake.createResetEncoderCommand())
-    //     .andThen(m_intake.createSetPositionCommand(0.2)));
+    // Shift Note further into Intake
+    new JoystickButton(m_operator, Button.kA.value)
+        .onTrue((m_intake.createResetEncoderCommand())
+        .andThen(m_intake.createSetPositionCommand(0.2)));
 
-    // // Shoot Note into Amp
-    // new JoystickButton(m_operator, Button.kY.value)
-    //     .whileTrue((m_intake.createSetVelocityCommand(1.0))
-    //     .onlyIf(raiseArmCommmand::isScheduled));
+    // Shoot Note into Amp
+    new JoystickButton(m_operator, Button.kY.value)
+        .whileTrue((m_intake.createSetVelocityCommand(1.0))
+        .onlyIf(raiseArmCommmand::isScheduled));
   }
   // spotless:on
 
@@ -95,7 +95,7 @@ public class RobotContainer {
   }
 
   public void teleopInit() {
-    //m_arm.createLowerArmCommand().schedule();
+    m_arm.createLowerArmCommand().schedule();
   }
 
   public void periodic() {
