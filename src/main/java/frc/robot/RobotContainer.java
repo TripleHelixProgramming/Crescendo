c0b92bb089c068b33f729bab4920f97b6feb7811
Nate Laverdure <nate.laverdure@gmail.com>
2024-01-29 10:54:07 -0500

    create Autonomous class to reduce code duplication

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.Alliance;
-import frc.robot.Constants.AutoConstants.Auto;
 import frc.robot.Constants.OIConstants;
 import frc.robot.arm.Arm;
 import frc.robot.drivetrain.Drivetrain;
@@ -27,12 +26,8 @@ public class RobotContainer {
   private Joystick m_driver = new Joystick(OIConstants.kDriverControllerPort);
   private XboxController m_operator = new XboxController(OIConstants.kOperatorControllerPort);
 
-  private Auto m_selectedAuto;
-
   // spotless:off
   public RobotContainer() {
-
-    m_selectedAuto = Auto.R_DRIVEFWD2M;
 
     m_swerve.setDefaultCommand(new ZorroDrive(m_swerve, m_driver, getAlliance()));
     m_swerve.configurePathPlanner();
@@ -73,28 +68,16 @@ public class RobotContainer {
   }
   // spotless:on
 
+  public Autonomous getSelectedAutonomous() {
+    return new Autonomous("R-driveFwd2m", Alliance.RED_ALLIANCE);
+  }
+
   public Command getAutonomousCommand() {
-    switch (m_selectedAuto) {
-      case B_DRIVEFWD2M:
-        return new PathPlannerAuto("B-driveFwd2m");
-      case R_DRIVEFWD2M:
-        return new PathPlannerAuto("R-driveFwd2m");
-      case B_DRIVERIGHTTURNTORIGHT:
-        return new PathPlannerAuto("B_SpinForward");
-      default:
-        return null;
-    }
+    return getSelectedAutonomous().getPathPlannerAuto();
   }
 
   public Alliance getAlliance() {
-    switch (m_selectedAuto) {
-      case R_DRIVEFWD2M:
-        return Alliance.RED_ALLIANCE;
-      case B_DRIVEFWD2M:
-      case B_DRIVERIGHTTURNTORIGHT:
-      default:
-        return Alliance.BLUE_ALLIANCE;
-    }
+    return getSelectedAutonomous().getAlliance();
   }
 
   public void teleopInit() {
@@ -103,5 +86,24 @@ public class RobotContainer {
 
   public void periodic() {
     SmartDashboard.putString("Alliance", getAlliance().toString());
+  }
+
+  private class Autonomous {
+
+    private final String filename;
+    private final Alliance alliance;
+
+    public Autonomous(String filename, Alliance alliance) {
+      this.filename = filename;
+      this.alliance = alliance;
+    }
+
+    public Command getPathPlannerAuto() {
+      return new PathPlannerAuto(filename);
+    }
+
+    public Alliance getAlliance() {
+      return alliance;
+    }
   }
 }
