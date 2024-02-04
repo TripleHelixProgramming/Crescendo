dbce0281553f4d383e7cdcc10ca15f0c9cd49883
DylanTaylor29 <132412179+DylanTaylor29@users.noreply.github.com>
2024-02-03 19:01:03 -0500

    worked on creating methods for climber

--- src/main/java/frc/robot/climber/Climber.java
+++ src/main/java/frc/robot/climber/Climber.java
@@ -3,10 +3,16 @@ package frc.robot.climber;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ClimberConstants;
+import frc.robot.Constants.OIConstants;
 
 public class Climber {
     private final CANSparkMax m_leftClimberMover;
@@ -17,6 +23,8 @@ public class Climber {
 
     private final SparkPIDController m_leftClimberPIDController;
     private final SparkPIDController m_rightClimberPIDController;
+
+    private final XboxController m_XboxController;
 
     public Climber() {
         m_leftClimberMover = new CANSparkMax(ClimberConstants.kLeftClimberMotorPort, MotorType.kBrushless);
@@ -53,5 +61,21 @@ public class Climber {
 
         m_leftClimberRelativeEncoder.setVelocityConversionFactor(ClimberConstants.kClimberVelocityConversionFactor);
         m_rightClimberRelativeEncoder.setVelocityConversionFactor(ClimberConstants.kClimberVelocityConversionFactor);
+
+        m_XboxController = new XboxController(OIConstants.kOperatorControllerPort);
+    }
+
+    private void setVelocity(double targetVelocity) {
+        m_leftClimberPIDController.setReference(targetVelocity, ControlType.kVelocity);
+        m_rightClimberPIDController.setReference(targetVelocity, ControlType.kVelocity);
+    }
+
+    private void setVoltage(double targetVoltage) {
+        m_leftClimberPIDController.setReference(targetVoltage, ControlType.kVoltage);
+        m_rightClimberPIDController.setReference(targetVoltage, ControlType.kVoltage);
+    }
+
+    public Command createSetVelocityCommand(double targetVelocity) {
+        
     }
 }
