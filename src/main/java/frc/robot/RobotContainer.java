ce00aa2d5fcf96df1b4fd33657bb894d14e8ed42
Nate Laverdure <nate.laverdure@gmail.com>
2024-02-24 16:59:53 -0500

    spotless

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -359,7 +358,6 @@ public class RobotContainer {
         m_arm.createCarryCommand(),
         m_intake.createAdvanceAfterIntakingCommand(),
         m_arm.createDeployCommand(),
-        new WaitCommand(1.9)
-        );
+        new WaitCommand(1.9));
   }
 }
