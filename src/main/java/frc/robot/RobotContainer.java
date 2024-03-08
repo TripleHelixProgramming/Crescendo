19abd613e17d60f9531a710e876b9b886a33a7f7
bot3O3 <75286864+bot3O3@users.noreply.github.com>
2024-03-08 17:15:26 -0500

    mirror and added auto with a return if it has picked up the piece

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,8 +2,11 @@
 
 package frc.robot;
 
+import java.util.function.IntSupplier;
+
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -38,7 +41,6 @@ import frc.robot.climber.commands.DriveToPositionCommand;
 import frc.robot.drivetrain.Drivetrain;
 import frc.robot.drivetrain.commands.ZorroDriveCommand;
 import frc.robot.intake.Intake;
-import java.util.function.IntSupplier;
 
 public class RobotContainer {
 
@@ -177,7 +179,7 @@ public class RobotContainer {
       case 4:
         m_autonomous = 
             m_swerve.redAllianceSupplier().getAsBoolean()
-                ? null 
+                ? new Autonomous("R-ThreePieceAutoTame") 
                 : new Autonomous("B-ThreePieceAutoTame");
         break;
 
